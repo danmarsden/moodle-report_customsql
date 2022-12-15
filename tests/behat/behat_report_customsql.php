2229466c40c3310f2ffc5927f287689d14ee4adb
Dan Marsden <dan@danmarsden.com>
2022-12-15 14:38:56 +1300

    Trim both fields.

--- tests/behat/behat_report_customsql.php
+++ tests/behat/behat_report_customsql.php
@@ -251,10 +251,11 @@ class behat_report_customsql extends behat_base {
     public function downloading_custom_sql_report_x_returns_a_file_with_headers(string $reportname, string $headers) {
         $report = $this->get_report_by_name($reportname);
         $url = new \moodle_url('/pluginfile.php/1/'.'report_customsql'. '/'.'download'. '/'. $report->id, ['dataformat' => 'csv']);
+        $headers = trim($headers);
 
         $session = $this->getSession()->getCookie('MoodleSession');
-        $filecontent = download_file_content($url, array('Cookie' => 'MoodleSession=' . $session));
-        if (trim($filecontent) != $headers) {
+        $filecontent = trim(download_file_content($url, array('Cookie' => 'MoodleSession=' . $session)));
+        if ($filecontent != $headers) {
             throw new \Behat\Mink\Exception\ExpectationException("File headers: $filecontent did not match expected: $headers", $this->getSession());
         }
     }
